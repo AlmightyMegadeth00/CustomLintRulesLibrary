7a5b7b2b73978b14e70291a7b55c29303cb6fe5b
Dave Kessler <dkessler@lesuto.com>
2025-10-01 18:43:16 -0500

    Add maven-publish setup

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.gradle.kotlin.dsl.compileOnly
 import org.gradle.kotlin.dsl.testImplementation
 
-
 plugins {
     id("java-library")
+    id("maven-publish")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.kotlinx.serialization)
 }
@@ -24,5 +24,18 @@ dependencies {
 tasks.jar {
     manifest {
         attributes(mapOf("Lint-Registry-v2" to "com.helpfullintrules.lint_rules.AndroidCustomLintIssueRegistry"))
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("android-lint-rules") {
+            groupId = "com.github.AlmightyMegadeth00"
+            artifactId = "AndroidCustomLintRules"
+            version = "1.0"
+
+            afterEvaluate {
+                artifact(tasks.jar)
+            }
+        }
     }
 }
